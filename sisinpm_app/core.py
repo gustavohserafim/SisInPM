b9d6a7b48a2482487d1e29b1f7c10ae0d3a59102
Gustavo Serafim <tioogu@gmail.com>
2023-08-24 22:55:28 -0300

    Update core.py

--- sisinpm_app/core.py
+++ sisinpm_app/core.py
@@ -11,6 +11,8 @@ bp = Blueprint("core", __name__, url_prefix="/")
 @bp.route("", methods=("GET", "POST"))
 @login_required
 def index():
+    from config import DB_USER, DB_PASS, DB_HOST, DB_SCHEMA
+    print("VARS:" + DB_USER, DB_PASS, DB_HOST, DB_SCHEMA)
     return render_template("core/index.html")
 
 
