4e9616cc0ef48bafd855cf76f3e8bf97f69c3511
Gustavo Serafim <tioogu@gmail.com>
2023-09-19 15:53:59 -0300

    Update core.py

--- sisinpm_app/core.py
+++ sisinpm_app/core.py
@@ -38,7 +38,8 @@ def avaliar():
 @bp.route("/avaliacao/todas", methods=["GET"])
 @login_required
 def get_avaliacoes():
-    return render_template("core/avaliacoes.html")
+    avaliacoes = CoreController.get_all_avaliacoes()
+    return render_template("core/avaliacoes.html", avalicoes_headers=avaliacoes[0].keys(), avaliacoes=avaliacoes)
 
 
 @bp.route("/healthcheck", methods=["GET"])
